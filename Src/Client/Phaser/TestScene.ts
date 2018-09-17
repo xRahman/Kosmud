c7f3070eb5f8a7f1ec3ab630ced2aa48d8754941
Rahman <Rahman@seznam.cz>
2018-09-17 15:48:48 +0200

    Experimenting with background image placement

--- Src/Client/Phaser/TestScene.ts
+++ Src/Client/Phaser/TestScene.ts
@@ -15,7 +15,17 @@ export class TestScene extends Phaser.Scene
 
   public create()
   {
-    this.add.tileSprite(0, 0, 1024, 768, 'background');
+    /// Size of background image 'deep_space0.jpg' is 3840x2400.
+    /// Origin of game coordinates is top-left. So placing
+    /// background imagte to 1920x1200 puts it to the top left
+    /// corner of the screen - which is not exacly what we want
+    /// (we want it to stretch over whole screen), but it's better
+    /// than to see just a quarter of it.
+    ///   Also we don't really need it to be tileSprite, because
+    /// background doesn't move (it represents far-away space).
+    this.add.tileSprite(1920, 1200, 3840, 2400, 'background');
+
+    /// At this moment I don't have a clue what these numbers mean.
     this.add.sprite(400, 500, 'ship');
   }
 
