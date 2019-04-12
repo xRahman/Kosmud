35c738f5ec4fbb5fcbc1924951cb8ce15daeed67
Rahman <Rahman@seznam.cz>
2019-04-12 22:59:26 +0200

    An angle is recomputed only if outside of valid range

--- Src/Shared/Utils/Angle.ts
+++ Src/Shared/Utils/Angle.ts
@@ -15,6 +15,14 @@ export namespace Angle
   // Makes sure that 'angle' is between 0 and 2π.
   export function zeroTo2Pi(angle: number): number
   {
+    // This check isn't technically needed but computing
+    // an angle which already is within valid range using
+    // the following formula might change it a little bit
+    // thanks to floating point rounding errors which can
+    // trigger false alarms.
+    if (angle >= 0 && angle < TWO_PI)
+      return angle;
+
     return (angle % TWO_PI + TWO_PI) % TWO_PI;
   }
 
